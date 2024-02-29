05216873f6bea6e5d52c75c86bbc344dbcfd371b
Theodor Tonum <theodor@tonum.no>
2024-02-29 19:12:33 +0100

    ref: initialize git repo for testing in dummy app folder

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,23 @@
 # Configure Rails Environment
 ENV["RAILS_ENV"] = "development"
+
+def system!(command, options = {})
+  system(command, {
+    exception: true,
+    out: ENV['CI'] ? $stdout : '/dev/null',
+    err: ENV['CI'] ? $stderr : '/dev/null',
+  }.merge(options))
+end
+
 Dir.chdir('test/dummy') do
+  # Initialize a new git repository in the dummy app
+  system!('rm -rf .git')
+  system!('git init --initial-branch=main .')
+  system!('git config user.email "you@example.com"')
+  system!('git config user.name "Your Name"')
+  system!('git add .')
+  system!('git commit -q -m "Initial commit"')
+
   # Make sure the database is setup in main
   system('bin/rails db:setup') || raise("Failed to setup database")
 end
