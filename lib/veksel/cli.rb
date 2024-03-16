5edac4cf62ec5b8be63956b567fd7c995f5286ee
Theodor Tonum <theodor@tonum.no>
2024-03-16 17:00:16 +0100

    ref: group private class methods

--- lib/veksel/cli.rb
+++ lib/veksel/cli.rb
@@ -2,43 +2,45 @@ require 'veksel'
 
 module Veksel
   module CLI
-    def self.suffix
-      print Veksel.suffix
-    end
-
-    def self.fork
-      return if Veksel.skip_fork?
-      t1 = Time.now.to_f
-
-      require 'veksel/commands/fork'
-      require 'psych'
-      require 'ostruct'
-      require 'fileutils'
-
-      config = read_config('config/database.yml')[:development]
-      if config.key?(:primary)
-        config = config[:primary]
-        $stderr.puts "Warning: Only your primary database will be forked"
+    class << self
+      def suffix
+        print Veksel.suffix
       end
 
-      target_database = config[:database] + Veksel.suffix
-      db = OpenStruct.new(configuration_hash: config, database: target_database)
-      Veksel::Commands::Fork.new(db).perform
+      def fork
+        return if Veksel.skip_fork?
+        t1 = Time.now.to_f
 
-      duration = ((Time.now.to_f - t1) * 1000).to_i
-      FileUtils.touch('tmp/restart.txt')
-      puts "Forked database in #{duration.to_i}ms"
-    end
+        require 'veksel/commands/fork'
+        require 'psych'
+        require 'ostruct'
+        require 'fileutils'
 
-    private_class_method def self.read_config(path)
-      raw_config = File.read(path)
+        config = read_config('config/database.yml')[:development]
+        if config.key?(:primary)
+          config = config[:primary]
+          $stderr.puts "Warning: Only your primary database will be forked"
+        end
 
-      if raw_config.include?('<%=')
-        require 'erb'
-        raw_config = ERB.new(raw_config).result
+        target_database = config[:database] + Veksel.suffix
+        db = OpenStruct.new(configuration_hash: config, database: target_database)
+        Veksel::Commands::Fork.new(db).perform
+
+        duration = ((Time.now.to_f - t1) * 1000).to_i
+        FileUtils.touch('tmp/restart.txt')
+        puts "Forked database in #{duration.to_i}ms"
       end
 
-      Psych.safe_load(raw_config, aliases: true, symbolize_names: true)
+      def read_config(path)
+        raw_config = File.read(path)
+
+        if raw_config.include?('<%=')
+          require 'erb'
+          raw_config = ERB.new(raw_config).result
+        end
+
+        Psych.safe_load(raw_config, aliases: true, symbolize_names: true)
+      end
     end
   end
 end
